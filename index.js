7ca34e2aaf85b1743368d2dbbc1e327a2af9da34
Sophia <sophierithous@gmail.com>
2019-02-15 14:34:32 +0000

    added index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+var buttons = document.querySelectorAll(".drum");
+for(var i = 0; i < buttons.length; i++){
+  buttons[i].addEventListener("click", function(){
+    var buttonIn = this.innerHTML;
+    makeSound(buttonIn);
+  });
+}
+
+document.addEventListener("keydown", function(e){
+  makeSound(event.key);
+})
+function makeSound(key){
+  switch (key) {
+    case "s":
+    var crash = new Audio("sounds/crash.mp3");
+    crash.play();
+      break;
+      case "o":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+        break;
+      case "p":
+       var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+          break;
+      case "h":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "i":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "a":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "e":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+    default:console.log('key was clicked');
+}
+}
